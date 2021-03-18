3f08d5f612def0e1ec9cc8d5ad4cb6bbbe8f8f4f
RavidSaadia <ravids1233@gmail.com>
2021-03-18 12:20:56 +0200

    add files

---
+++ Board.cpp
@@ -0,0 +1,28 @@
+//
+// Created by ravid on 17/03/2021.
+//
+
+#include "Board.hpp"
+
+using namespace ariel{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Board.hpp
@@ -0,0 +1,17 @@
+//
+// Created by ravid on 17/03/2021.
+//
+
+#ifndef EX2_BOARD_H
+#define EX2_BOARD_H
+namespace ariel {
+    void post (int row, int colum, Direction direction, int length);
+
+
+
+    class Board {
+
+    };
+}
+
+#endif //EX2_BOARD_H
